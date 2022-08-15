425acf18440d02e34c518f561631f9b8f412ca43
Lais Furtado <97700608+laisslf@users.noreply.github.com>
2022-08-15 19:02:20 -0400

    Update data.js

--- data.js
+++ data.js
@@ -15,67 +15,67 @@ var petData = [
     description: "<span>Branca</span> was our pretty queen. Unfortunately, she died last year after a rough battle against breast cancer.",
     type: "branca"
   }, {
-    image: { src: "Photos/Gatinho1.jpg", alt: "Gatinho", width: 120, height: 160 },
+    image: { src: "../Photos/Gatinho1.jpg", alt: "Gatinho", width: 120, height: 160 },
     name: "Gatinho",
     description: "<span>Gatinho</span> was our firecracker. He also died a few years ago. His death shook us. He was alright and suddenly he had a kidney failure.",
     type: "gatinho"
   }, {
-    image: { src: "Photos/Gato2.jpg", alt: "Gato", width: 120, height: 160 },
+    image: { src: "../Photos/Gato2.jpg", alt: "Gato", width: 120, height: 160 },
     name: "Gato",
     description: "Gato is our buddy. He loves to cuddle. He can be a little bit mean, but overall is the perfect cat.",
     type: "gato"
   }, {
-    image: { src: "Photos/Outro2.jpg", alt: "Outro Gato", width: 120, height: 160 },
+    image: { src: "../Photos/Outro2.jpg", alt: "Outro Gato", width: 120, height: 160 },
     name: "Outro Gato",
     description: "Our tabby fat boy is always giving us the side eye. He is currently on a strict diet. He's got to lose 2 kilos.",
     type: "outro"
   }, {
-    image: { src: "Photos/Branca2.jpg", alt: "Branca", width: 120, height: 160 },
+    image: { src: "../Photos/Branca2.jpg", alt: "Branca", width: 120, height: 160 },
     name: "Branca",
     description: "This lady was all about purring and sleeping. Never disappointed us. Always the perfect queen. She could be mean and scratch you, but probably you deserved it.",
     type: "branca"
   }, {
-    image: { src: "Photos/Gatinho2.jpg", alt: "Gatinho", width: 120, height: 160 },
+    image: { src: "../Photos/Gatinho2.jpg", alt: "Gatinho", width: 120, height: 160 },
     name: "Gatinho",
     description: "He was an actor. He could seduce you with his eyes. You just couldn't resist his lovely face.",
     type: "gatinho"
   }, {
-    image: { src: "Photos/Gato3.jpg", alt: "Gato", width: 120, height: 160 },
+    image: { src: "../Photos/Gato3.jpg", alt: "Gato", width: 120, height: 160 },
     name: "Gato",
     description: "<span>Gato</span> in Portuguese means cat. And that's it. You just can't forget.",
     type: "gato"
   }, {
-    image: { src: "Photos/Outro3.jpg", alt: "Outro Gato", width: 120, height: 160 },
+    image: { src: "../Photos/Outro3.jpg", alt: "Outro Gato", width: 120, height: 160 },
     name: "Outro Gato",
     description: "Outro Gato in Portuguese means <span>Another Cat</span>. It seems that we are no creative, but they are a success with the vets.",
     type: "outro"
   }, {
-    image: { src: "Photos/Branca3.jpg", alt: "Branca", width: 120, height: 160 },
+    image: { src: "../Photos/Branca3.jpg", alt: "Branca", width: 120, height: 160 },
     name: "Branca",
     description: "<span>Branca</span> means white. I guess you can understand where this is going.",
     type: "branca"
   }, {
-    image: { src: "Photos/Gatinho3.jpg", alt: "Gatinho", width: 120, height: 160 },
+    image: { src: "../Photos/Gatinho3.jpg", alt: "Gatinho", width: 120, height: 160 },
     name: "Gatinho",
     description: "Gatinho means <span>small cat</span>. He was really small compared to the others. But he made enough noise for 5 cats.",
     type: "gatinho"
   }, {
-    image: { src: "Photos/Gato4.jpg", alt: "Gato", width: 120, height: 160 },
+    image: { src: "../Photos/Gato4.jpg", alt: "Gato", width: 120, height: 160 },
     name: "Gato",
     description: "<span>Gato</span> has only to activities: sleep or meow to the window. He is a lot of fun. Really, he is. Right now he is checking and judging this page.",
     type: "gato"
   }, {
-    image: { src: "Photos/Outro4.jpg", alt: "Outro Gato", width: 120, height: 160 },
+    image: { src: "../Photos/Outro4.jpg", alt: "Outro Gato", width: 120, height: 160 },
     name: "Outro Gato",
     description: "Outro Gato was left by his mother on our garden when he was a few months old. It took him 8 years to finally accept he is an indoor cat and that humans are ok. Now, he sleeps in the bed and loves when we rub his belly.",
     type: "outro"
   }, {
-    image: { src: "Photos/Branca4.jpg", alt: "Branca", width: 120, height: 160 },
+    image: { src: "../Photos/Branca4.jpg", alt: "Branca", width: 120, height: 160 },
     name: "Branca",
     description: "I really miss her. She was such a delight. I'm happy that I got her out of the streets and could give her some lovely 6 years of cuddles and pets.",
     type: "branca"
   }, {
-    image: { src: "Photos/Gatinho4.jpg", alt: "Gatinho", width: 120, height: 160 },
+    image: { src: "../Photos/Gatinho4.jpg", alt: "Gatinho", width: 120, height: 160 },
     name: "Gatinho",   
     description: "My sassy feline. He was friendly, fun, and so cute. He will be forever missed.",
     type: "gatinho"
